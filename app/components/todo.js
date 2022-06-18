8d18094c669f8c9e88cca543b68281b88d3fafb8
msaaddev <mrsaadirfan@gmail.com>
2022-06-19 01:09:25 +0500

    📦 NEW: Added create todo, remove, and clear all todo features

--- app/components/todo.js
+++ app/components/todo.js
@@ -1,3 +1,33 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class TodoComponent extends Component {}
+export default class TodoComponent extends Component {
+  @tracked newTodo = '';
+  @tracked todos = [
+    'delectus aut autem',
+    'quis ut nam facilis et officia qui',
+    'fugiat veniam minus',
+    'et porro tempora',
+    'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    'qui ullam ratione quibusdam voluptatem quia omnis',
+    'illo expedita consequatur quia in',
+  ];
+
+  get todoList() {
+    return this.todos;
+  }
+
+  @action createTodo(e) {
+    e.preventDefault();
+    this.todos = this.todos.concat(this.newTodo);
+  }
+
+  @action deleteTodo(todo) {
+    this.todos = this.todos.filter((item) => item !== todo);
+  }
+
+  @action clearAll() {
+    this.todos = [];
+  }
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {
-  async model() {
-    try {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/todos'
-      );
-      let todos = await response.json();
-      todos = todos.slice(0, 10);
-      return todos;
-    } catch (err) {
-      console.log(err);
-    }
-  }
-}
+export default class IndexRoute extends Route {}
